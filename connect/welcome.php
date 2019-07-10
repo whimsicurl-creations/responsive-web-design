43f5b90a114da351324fcd6dbe27fba82eaa30d1
Bethany <blmeier@gmail.com>
2019-07-10 15:08:59 -0400

    custom php

---
+++ connect/welcome.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+
+    Welcome <?php echo $_POST["name"]; ?> 
+
+    <p>Your email is <?php echo $_POST["email"]; ?></p>
+
+    <p>Your favorite URL resource is: <?php echo $_POST["url"]; ?></p>
+
+    <p>Your favorite color is: <?php echo $_POST["color"]; ?></p>
+
+    <p>Your favorite season is: <?php echo $_POST["season"]; ?></p>
+
+    <p>Your guess for my favorite number is: <?php echo $_POST["number"]; ?></p>
+
+</body>
+
+</html>
